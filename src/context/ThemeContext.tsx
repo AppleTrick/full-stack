484071323064dfae5a315b9aa75a211fd450a285
ChangHee Park <arrans9591@gmail.com>
2024-05-14 17:34:48 +0900

    주석 작성

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -7,8 +7,10 @@ interface ThemeContextType {
   toggle: () => void;
 }
 
+// 컨텍스트 초기값 설정
 export const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
 
+// 로컬 스토리지에서 데이터를 가지고 옴 만약에 데이터가 없을경우 값을 생성
 const getFromLocalStorage = () => {
   if (typeof window !== "undefined") {
     const value = localStorage.getItem("theme");
@@ -22,12 +24,15 @@ interface ThemeContextProviderProps {
 }
 
 export const ThemeContextProvider: React.FC<ThemeContextProviderProps> = ({ children }) => {
+  // 현재 테마의 상태관리
   const [theme, setTheme] = useState<string>(getFromLocalStorage);
 
+  // toogle 클릭시 값을 변환
   const toggle = () => {
     setTheme(theme === "light" ? "dark" : "light");
   };
 
+  //theme 상태가 변경될 때마다 이를 로컬 스토리지에 저장하여, 사용자의 테마 선택을 유지
   useEffect(() => {
     localStorage.setItem("theme", theme);
   }, [theme]);
