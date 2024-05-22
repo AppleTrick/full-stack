ac45145d53aa23366be94538bf0f5f4069de6702
“ParkChangHee” <arrans9591@gmail.com>
2024-05-22 20:16:15 +0900

    내용 진행

--- src/components/cardlist/CardList.tsx
+++ src/components/cardlist/CardList.tsx
@@ -1,9 +1,17 @@
 import Pagenation from "../pagenation/Pagenation";
 import styles from "./cardlist.module.css";
+import Card from "../card/Card";
 
 const CardList = () => {
   return (
     <div className={styles.container}>
+      <h1 className={styles.title}>Recent Posts</h1>
+      <div className={styles.posts}>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
       <Pagenation />
     </div>
   );
